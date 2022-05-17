29402b117b84dd9a45d03196c61da33ca8523a20
sunhey kim <jinxianhuijxh@naver.com>
2022-05-17 19:38:18 +0900

    실습 ㅇ5-2완성

--- app/src/main/java/kr/hs/emirim/mirim_0512/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/mirim_0512/MainActivity.java
@@ -3,9 +3,11 @@ package kr.hs.emirim.mirim_0512;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
@@ -16,5 +18,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        textResult = findViewById(R.id.text_resurt);
+        Button btnplus = findViewById(R.id.btn_plus);
+        Button btnMinus = findViewById(R.id.btn_minus);
+        Button btnMulti = findViewById(R.id.btn_crose);
+        Button btnDivide = findViewById(R.id.btn_divide);
+
+        for(int i=0; i<btnNum.length; i++){
+            final  int index = i;
+            btnNum[i] = findViewById(btnNumId[i]);
+            btnNum[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String s = " ";
+                    if(edit1.isFocused()){
+                        s = edit1.getText().toString()+btnNum[index].getText();
+                        edit1.setText(s);
+                    }else if(edit2.isFocused()){
+                        s = edit2.getText().toString()+btnNum[index].getText();
+                        edit2.setText(s);
+                    }else {
+                        Toast.makeText(getApplicationContext(), "editText에 포커스가 없다", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        btnplus.setOnClickListener(btnListener);
+        btnMinus.setOnClickListener(btnListener);
+        btnMulti.setOnClickListener(btnListener);
+        btnDivide.setOnClickListener(btnListener);
+
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int n1 = Integer.parseInt(edit1.getText().toString());
+            int n2 = Integer.parseInt(edit2.getText().toString());
+            int result = 0;
+            switch (view.getId()){
+                case R.id.btn_plus:
+                    result = n1+n2;
+                        break;
+                case R.id.btn_minus:
+                    result = n1-n2;
+                    break;
+                case R.id.btn_crose:
+                    result = n1*n2;
+                    break;
+                case R.id.btn_divide:
+                    result = n1/n2;
+                    break;
+            }
+            textResult.setText("결과: "+result);
+        }
+    };
 }
